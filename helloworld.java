81491b637d40a2ef74af7112ed501655761be24e
sachinmodey <115960580+sachinmodey@users.noreply.github.com>
2023-01-30 21:32:50 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - Jan 30th - 08:30 AM - triggered by sachin");
+    System.out.println("Hello World - Jan 30th - 21:32 PM - triggered by sachin");
     }
 }
